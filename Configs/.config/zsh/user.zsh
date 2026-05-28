ente0/HyDE

commit 9851202d43461dd14d5378471b0ed9b4a198a58a

Uncomment fastfetch command in user.zsh

Configs/.config/zsh/user.zsh
@@ -7,11 +7,11 @@
 #        pokego --no-title -r 1,3,6
 #    elif command -v pokemon-colorscripts >/dev/null; then
 #        pokemon-colorscripts --no-title -r 1,3,6
-#    elif command -v fastfetch >/dev/null; then
-#        if do_render "image"; then
-#            fastfetch --logo-type kitty
-#        fi
-#    fi
+    elif command -v fastfetch >/dev/null; then
+        if do_render "image"; then
+            fastfetch --logo-type kitty
+        fi
+    fi
 #fi
 
 #   Overrides 

commit 5b7643cc3925f51b1bb3603b221de863172ed5e4

Fix orphan elif in user.zsh causing zsh parse error
Commented if-block left elif/fi uncommented (regression from 9851202d).
Collapsed the dead pokego/pokemon-colorscripts branches and kept the
fastfetch interactive-shell guard.

Configs/.config/zsh/user.zsh
@@ -1,18 +1,14 @@
 #  Startup 
 # Commands to execute on startup (before the prompt is shown)
 # Check if the interactive shell option is set
-#if [[ $- == *i* ]]; then
+if [[ $- == *i* ]]; then
     # This is a good place to load graphic/ascii art, display system information, etc.
-#    if command -v pokego >/dev/null; then
-#        pokego --no-title -r 1,3,6
-#    elif command -v pokemon-colorscripts >/dev/null; then
-#        pokemon-colorscripts --no-title -r 1,3,6
-    elif command -v fastfetch >/dev/null; then
+    if command -v fastfetch >/dev/null; then
         if do_render "image"; then
             fastfetch --logo-type kitty
         fi
     fi
-#fi
+fi
 
 #   Overrides 
 # HYDE_ZSH_NO_PLUGINS=1 # Set to 1 to disable loading of oh-my-zsh plugins, useful if you want to use your zsh plugins system 